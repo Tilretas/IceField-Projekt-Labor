871645e5810425062ead493b2a141c8a8c7e75a0
balgrober <groberbalint@gmail.com>
2020-04-28 01:53:59 +0200

    Update Rope.java

--- src/game/Rope.java
+++ src/game/Rope.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.util.Scanner;
+
 public class Rope implements Item
 {
 	public void used(Piece p)
@@ -9,8 +11,13 @@ public class Rope implements Item
 	
 	public Piece getPiece()
 	{
-		//return Game.getInstance().getBoard().getTiles().get().getPlayersOnTile().get();
-		return new Eskimo();
+		Scanner scr = new Scanner(System.in);
+		System.out.println("Where is the piece you want to save? (Tile index 0-24):");
+		int location = scr.nextInt();
+		System.out.println("Which piece do you want to save?");
+		int piece = scr.nextInt();
+		
+		return Game.getInstance().getBoard().getTiles().get(location).getPieces().get(piece);
 	}
 	
 	private void savePiece(Piece p)
