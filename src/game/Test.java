3f827828cebbcda682eab89a3b38fa43dcd8a72b
Gábor Juhász <jgabor1045@gmail.com>
2020-04-28 00:15:05 +0200

    Update Test.java

--- src/game/Test.java
+++ src/game/Test.java
@@ -1,21 +1,20 @@
 package game;
 
+import java.io.*;
+import java.util.*;
+
 public class Test
 {
 	private boolean exit;
     private boolean running;
-
+    private boolean save = false;
+    private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     
     private Test()
     {
     	exit = false;
     	running = false;
     	//...
-    }
-    
-    public static void main(String[] args)
-    {
-    	Test test = new Test();
     }
     
     private void run()
@@ -26,5 +25,22 @@ public class Test
     	}
     }
     
+    private List<String> ReadLine()
+    {
+        String line;
+        List<String> input;
+        try
+        {
+            line = br.readLine();
+            if (line != null)
+            {
+                input = Arrays.asList(line.split(" "));
+                return input;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
