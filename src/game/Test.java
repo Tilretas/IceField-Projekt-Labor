1b22b4e6deb0eacd25c651117cc0144a4aefacf0
Gábor Juhász <jgabor1045@gmail.com>
2020-04-27 22:18:11 +0200

    Creating Test class

---
+++ src/game/Test.java
@@ -0,0 +1,30 @@
+package game;
+
+public class Test
+{
+	private boolean exit;
+    private boolean running;
+
+    
+    private Test()
+    {
+    	exit = false;
+    	running = false;
+    	//...
+    }
+    
+    public static void main(String[] args)
+    {
+    	Test test = new Test();
+    }
+    
+    private void run()
+    {
+    	while(!exit)
+    	{
+    		
+    	}
+    }
+    
+
+}
