b68c97450613ad2811e2e2de25ace4c3235af3b0
Tilretas <badampeter@gmail.com>
2020-05-17 17:31:38 +0200

    ass

--- src/graphics/View.java
+++ src/graphics/View.java
@@ -6,10 +6,6 @@ import java.util.ArrayList;
 
 public class View
 {
-	private ArrayList<Drawable> tiles;
-	private ArrayList<Drawable> movables;
-	private ArrayList<Drawable> shelters;
-	private BufferedImage buffer;
 	private boolean valid;
 	
 	public void paintComponent(Graphics g)
@@ -18,21 +14,6 @@ public class View
 	}
 	
 	public void clear()
-	{
-		
-	}
-	
-	public void addTile(Drawable d)
-	{
-		
-	}
-	
-	public void addMovable(Drawable d)
-	{
-		
-	}
-	
-	public void addShelter(Drawable d)
 	{
 		
 	}
