49ca0859456ef15306b8bff9530edf1e5ec69f4f
Tilretas <badampeter@gmail.com>
2020-05-17 17:01:42 +0200

    cf hvg

--- src/graphics/MouseEventHandler.java
+++ src/graphics/MouseEventHandler.java
@@ -1,11 +1,42 @@
 package graphics;
 
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class MouseEventHandler implements MouseListener
 {
 	public void onClick()
 	{
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 }
