fdb6782885cc722e65efb391ed7d8e88ab708ccf
Kaposi Adam <kaposi.adam@gmail.com>
2020-05-17 17:31:09 +0200

    TileGraphics

--- src/graphics/TileGraphics.java
+++ src/graphics/TileGraphics.java
@@ -1,5 +1,18 @@
 package graphics;
 
 public enum TileGraphics {
-
+	Ice, 
+	IceIgloo, 
+	IceTent, 
+	IceBear, 
+	IceBearIgloo, 
+	IceBearTent, 
+	Item, 
+	ItemIgloo, 
+	ItemTent, 
+	ItemBear, 
+	ItemBearIgloo, 
+	ItemBearTent, 
+	Hole, 
+	HoleBear
 }
